fc2987482cc62f56cb4203e97a50f2d909e75137
TEI-Sandbox <derekv@trendsinc.com>
2026-04-29 12:27:26 -0700

    Use Shopify auth redirect from root route

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -1,6 +1,6 @@
-import { redirect } from "@remix-run/node";
+import { authenticate } from "../shopify.server";
 
 export const loader = async ({ request }) => {
-  const url = new URL(request.url);
-  return redirect(`/app${url.search}`);
+  const { redirect } = await authenticate.admin(request);
+  return redirect("/app");
 };
